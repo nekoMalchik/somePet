ac2ca64d9bf43347c122503aa8ec2ba0785b1bdf
dstartsev <mityastar01@gmail.com>
2026-06-22 20:40:23 +0500

    PdoSessionHandler

---
+++ migrations/Version20260622000000.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20260622000000 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->addSql('CREATE TABLE sessions (
+            sess_id VARCHAR(128) NOT NULL PRIMARY KEY,
+            sess_data BYTEA NOT NULL,
+            sess_lifetime INTEGER NOT NULL,
+            sess_time INTEGER NOT NULL
+        )');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP TABLE sessions');
+    }
+}
